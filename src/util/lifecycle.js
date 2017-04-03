e7812dae3beb2d7e0b0a4eeb88cca0d633884be0
Parashuram <code@nparashuram.com>
2017-04-03 11:09:54 -0700

    Fixing react native eject command

--- src/util/lifecycle.js
+++ src/util/lifecycle.js
@@ -22,5 +22,5 @@ export async function createReactNativeApp() {
   log('Bootstrapping React Native in', dirs.testAppBin);
   await promisify(fs.copy, fs, dirs.testAppSrc, dirs.testAppBin);
   await promisify(child_process.exec, child_process, `yarn install`, { cwd: dirs.testAppBin });
-  await promisify(child_process.exec, child_process, 'node_modules/.bin/react-native eject', { cwd: dirs.testAppBin });
+  await promisify(child_process.exec, child_process, 'node node_modules/react-native/local-cli/cli.js eject', { cwd: dirs.testAppBin });
 }
