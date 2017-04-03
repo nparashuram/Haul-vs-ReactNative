6d847cec377f1db38d59f9ad61d78b343282c2d8
Parashuram <code@nparashuram.com>
2017-04-02 21:49:14 -0700

    Added test case to add component trees

--- src/cases/adding-child-trees.js
+++ src/cases/adding-child-trees.js
@@ -22,8 +22,12 @@ export default testCase('Adding One Component at a time', (setup, test, cleanup)
     for (var i = 1; i < 30; i++) {
       await sleep(5000);
       await createComponent(`Component${i}`);
+      for (var j = 0; j < 20; j++) {
+        await createComponent(`ComponentTree_${i}_${j}`);
+        await addChildComponent(`Component${i}`, `ComponentTree_${i}_${j}`);
+      }
       await addChildComponent(`Component${i - 1}`, `Component${i}`);
-      log('Added Component ', i);
+      log('Added ComponentTree ', i);
       await sleep(5000);
       await packager.fetchBundle();
       addResult(i, hrtimer.results[i]);
@@ -31,3 +35,4 @@ export default testCase('Adding One Component at a time', (setup, test, cleanup)
   });
 
 });
+
--- src/cases/index.js
+++ src/cases/index.js
@@ -1,6 +1,7 @@
 const cases = {};
 [
-  require('./adding-one-component')
+  require('./adding-one-component'),
+  require('./adding-child-trees')
 ].forEach(c => cases[c.default.name] = c.default.fn);
 
 export default cases;
